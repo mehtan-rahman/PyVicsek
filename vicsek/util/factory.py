b80ca5ee962f635abe791aa2916ca8b0e06b018e
mehtan-rahman <mehtan.rahman@utexas.edu>
2024-12-04 01:29:58 -0600

    seed for reproducibility

--- vicsek/util/factory.py
+++ vicsek/util/factory.py
@@ -2,17 +2,25 @@ import numpy as np
 from vicsek.models.particle import Particle
 
 
-def initialize_random_particles(n_particles: int, box_length: float, speed: float, n_dimensions: int):
+def initialize_random_particles(
+        n_particles: int,
+        box_length: float,
+        speed: float,
+        n_dimensions: int,
+        particle_type: str = 'particle',
+        seed: int = None,
+):
     particles = []
+    rng = np.random.default_rng(seed)
 
     for i in range(n_particles):
-        position = np.random.uniform(0, box_length, size=n_dimensions)
-        velocity = speed * np.random.uniform(-1, 1, size=n_dimensions)
+        position = rng.uniform(0, box_length, size=n_dimensions)
+        velocity = speed * rng.uniform(-1, 1, size=n_dimensions)
 
         particles.append(Particle(
             position=position,
             velocity=velocity,
             name=f"p{i}",
-            type="standard"
+            type=particle_type
         ))
     return particles
