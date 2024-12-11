36fffd8f0bef2fd4992ca4a6911d4811240fe368
mehtan-rahman <mehtan.rahman@utexas.edu>
2024-12-10 20:44:31 -0600

    PEP8 formatting

--- tests/test_cell_list.py
+++ tests/test_cell_list.py
@@ -3,14 +3,16 @@ import numpy as np
 from vicsek.models.particle import Particle
 from vicsek.util.cell_list import CellList
 
+
 @pytest.fixture
 def particles():
     return [
-        Particle(position=np.array([0.5, 0.5]), velocity=np.array([0,0]), name="A", type="A"),
-        Particle(position=np.array([0.6, 0.6]), velocity=np.array([0,0]), name="B", type="B"),
-        Particle(position=np.array([1.5, 1.5]), velocity=np.array([0,0]), name="C", type="C"),
-        Particle(position=np.array([5.0, 5.0]), velocity=np.array([0,0]), name="D", type="D")
+        Particle(position=np.array([0.5, 0.5]), velocity=np.array([0, 0]), name="A", type="A"),
+        Particle(position=np.array([0.6, 0.6]), velocity=np.array([0, 0]), name="B", type="B"),
+        Particle(position=np.array([1.5, 1.5]), velocity=np.array([0, 0]), name="C", type="C"),
+        Particle(position=np.array([5.0, 5.0]), velocity=np.array([0, 0]), name="D", type="D")
     ]
+
 
 def test_initialize(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0)
@@ -20,20 +22,23 @@ def test_initialize(particles):
     assert cell_list.n_cells == 10
     assert cell_list.cell_size == 1.0
 
+
 def test_build(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0)
     cell_list.build()
     assert len(cell_list.cells) == 100
-    assert len(cell_list.cells[(9,9)]) == 2
-    assert len(cell_list.cells[(8,8)]) == 1
+    assert len(cell_list.cells[(9, 9)]) == 2
+    assert len(cell_list.cells[(8, 8)]) == 1
+
 
 def test_update(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0)
     cell_list.build()
     particles[0].position = np.array([1.5, 1.5])
     cell_list.update()
-    assert len(cell_list.cells[(9,9)]) == 1
-    assert len(cell_list.cells[(8,8)]) == 2
+    assert len(cell_list.cells[(9, 9)]) == 1
+    assert len(cell_list.cells[(8, 8)]) == 2
+
 
 def test_neighbors_pbc(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0, use_pbc=True)
@@ -41,6 +46,7 @@ def test_neighbors_pbc(particles):
     neighbors = cell_list.get_neighbors(particles[0])
     assert len(neighbors) == 1
     assert neighbors[0].name == "B"
+
 
 def test_neighbors_no_pbc(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0, use_pbc=False)
@@ -52,6 +58,7 @@ def test_neighbors_no_pbc(particles):
     particles[0].position = np.array([-0.5, -0.5])
     cell_list.update()
     assert len(cell_list.get_neighbors(particles[0])) == 0
+
 
 def test_visualize(particles):
     cell_list = CellList(particles, box_length=10.0, interaction_range=1.0)
